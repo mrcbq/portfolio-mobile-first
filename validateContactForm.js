eef28b6dc92d4b969af5f628801acb85143ed4cd
mrcbq <marcobaquero@gmail.com>
2023-04-14 16:18:17 -0500

    Add a Contact Form Validation JS

---
+++ validateContactForm.js
@@ -0,0 +1,17 @@
+const mail = document.querySelector('#mail');
+const form = document.querySelector('.contact-form');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const mailValue = mail.value;
+  const mailLowerCase = mailValue.toLowerCase();
+  const alertMsg = document.querySelector('#alert-msg');
+
+  if (mailValue === mailLowerCase) {
+    alertMsg.style.display = 'inline';
+    form.submit();
+  } else {
+    alertMsg.innerHTML = `The Email must be all in minus, This is your mail in a valid format: ${mailLowerCase}`;
+    alertMsg.style.display = 'inline';
+  }
+});
