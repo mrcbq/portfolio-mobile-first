73112709bbe353fc6f8bc7884fc3d903a87f314b
mrcbq <marcobaquero@gmail.com>
2023-04-14 16:26:25 -0500

    Add the validation logic for the mail, compare is the mail is in minus

--- validateContactForm.js
+++ validateContactForm.js
@@ -10,8 +10,6 @@ form.addEventListener('submit', (event) => {
   if (mailValue === mailLowerCase) {
     alertMsg.style.display = 'inline';
     form.submit();
-  } else {
-    alertMsg.innerHTML = `The Email must be all in minus, This is your mail in a valid format: ${mailLowerCase}`;
-    alertMsg.style.display = 'inline';
-  }
+  } 
+  
 });
