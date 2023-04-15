7e9333dc9298d266cc11cc7f25a35d748fa23433
mrcbq <marcobaquero@gmail.com>
2023-04-14 20:51:41 -0500

    Update validateContactForm.js
    Using the same code from the last aproved pr  for validate contact form
    
    Co-Authored-By: Gardimy Charles <gardimycharles889@gmail.com>

--- validateContactForm.js
+++ validateContactForm.js
@@ -10,5 +10,9 @@ form.addEventListener('submit', (event) => {
   if (mailValue === mailLowerCase) {
     alertMsg.style.display = 'inline';
     form.submit();
+  } else {
+    alertMsg.innerHTML = `The Email must be in lowercase, This is your mail in a valid format:
+     ${mailLowerCase}`;
+    alertMsg.style.display = 'inline';
   }
 });
